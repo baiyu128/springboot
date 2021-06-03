f01fec7c8ca00001e76656b7741d7c18973a9390
baiyu128 <baixixi187@163.com>
2021-06-03 10:54:24 +0800

    增加空值判断

--- security-jwt/src/main/java/com/baiyu/securityjwt/filter/JwtTokenFilter.java
+++ security-jwt/src/main/java/com/baiyu/securityjwt/filter/JwtTokenFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -37,7 +38,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         String token = request.getHeader( HEADER_STRING );
-        if (null != token) {
+        if (!StringUtils.isEmpty(token)) {
             String username = jwtTokenUtil.getUsernameFromToken(token);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
