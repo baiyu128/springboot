78c4a38a18663084b9e6fd25344353fd1428b3a6
baiyu128 <baixixi187@163.com>
2021-07-02 10:58:55 +0800

    java-utils

---
+++ java-utils/src/main/java/com/baiyu/utools/Test.java
@@ -0,0 +1,17 @@
+package com.baiyu.utools;
+
+/**
+ * class Name:
+ *
+ * @author yx
+ * @description :
+ * @date 2021/6/30
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        long i ;
+        i = 1__3___2__4__0;
+        System.out.println(i);
+    }
+}
