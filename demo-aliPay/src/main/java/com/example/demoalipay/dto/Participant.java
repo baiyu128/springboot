624fa3ac808edaee458b583c9ac7d451d4bd9a18
baiyu128 <baixixi187@163.com>
2022-05-03 14:31:30 +0800

    dto

---
+++ demo-aliPay/src/main/java/com/example/demoalipay/dto/Participant.java
@@ -0,0 +1,32 @@
+package com.example.demoalipay.dto;
+
+import lombok.Data;
+
+@Data
+public class Participant {
+    /**
+     * 参与方的唯一标识
+     */
+    private String identity;
+
+    public String getIdentity_type() {
+        return identity_type;
+    }
+
+    public void setIdentity_type(String identity_type) {
+        this.identity_type = identity_type;
+    }
+
+    /**
+     * 参与方的标识类型，目前支持如下类型：
+     * 1、ALIPAY_USER_ID 支付宝的会员ID
+     * 2、ALIPAY_LOGON_ID：支付宝登录号，支持邮箱和手机号格式
+     */
+    private String identity_type;
+ 
+    /**
+     * 参与方真实姓名，如果非空，将校验收款支付宝账号姓名一致性。
+     * 当identity_type=ALIPAY_LOGON_ID时，本字段必填。
+     */
+    private String name;
+}
