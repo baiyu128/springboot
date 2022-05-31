1b532e51b1c5b1de8a62269fd04545c83536c86c
baiyu128 <baixixi187@163.com>
2022-05-31 16:13:45 +0800

    start class

---
+++ demo-aliPay/src/main/java/com/example/demoalipay/DemoAliPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoalipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAliPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAliPayApplication.class, args);
+    }
+
+}
