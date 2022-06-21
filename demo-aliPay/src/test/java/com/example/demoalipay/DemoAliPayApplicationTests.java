c7598b66739e021187ac181936cef0c7d1fe22cf
baiyu128 <baixixi187@163.com>
2022-06-21 10:15:07 +0800

    add test file

---
+++ demo-aliPay/src/test/java/com/example/demoalipay/DemoAliPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoalipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAliPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
