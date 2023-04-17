99e104751c91c0d00b1d00bbe4773fd89fe9a4e8
baiyu128 <baixixi187@163.com>
2023-04-17 09:08:07 +0800

    xss

---
+++ springboot-xss-filter/src/main/java/com/baiyu/springbootxssfilter/SpringbootXssFilterApplication.java
@@ -0,0 +1,13 @@
+package com.baiyu.springbootxssfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXssFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootXssFilterApplication.class, args);
+    }
+
+}
